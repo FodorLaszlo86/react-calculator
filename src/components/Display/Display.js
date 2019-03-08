17d653e83b838803b0692c65a8cf798b3023204f
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-03-08 19:52:11 +0100

    displayed correct info, if reset displays only 0

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -2,13 +2,19 @@ import React from 'react';
 import classes from './Display.css';
 
 
-const display = (props) => (
+const display = ({ formula, memory, currentEl }) => (
     <div className={ classes.CalcDisplay }>
         <div className={ classes.SecondaryDisplay }>
-            <div className={ classes.MemoryDisplay }></div>
-            <div className={ classes.ProcessDisplay }>3 + 2 - 11</div>
+            <div className={ classes.MemoryDisplay }>
+                { memory !== '' ? `M: ${ memory }` : null }
+            </div>
+            <div className={ classes.ProcessDisplay }>
+                { formula.length > 0 ? formula.join(' ') : null }
+            </div>
         </div>
-        <div id="display" className={ classes.MainDisplay }>0</div>
+        <div id="display" className={ classes.MainDisplay }>
+            { currentEl === '' ? '0' : currentEl }
+        </div>
     </div>
 )
 

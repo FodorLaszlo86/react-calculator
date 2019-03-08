d600c1a2e165b17ad6ad17b2294c77a6d3beb908
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-03-08 19:55:22 +0100

    reset calculator function

--- src/containers/Calculator/Calculator.js
+++ src/containers/Calculator/Calculator.js
@@ -4,23 +4,59 @@ import Buttons from '../../components/Buttons/Buttons';
 import Display from '../../components/Display/Display';
 import Header from '../../components/Header/Header';
 
+/*
+
+
+*/
+
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        formula: [],
-        memory: '',
-        equalityPress: false
+        formula: ['33', '*', '12', '+'],
+        memory: '2.085',
+        currentEl: '13.56',
+        result: null,
+        equalityPressed: false
     }
   }
+
+
+  handleCalculator = (e) => {
+    const btnVal = e.target.textContent;
+
+    switch (btnVal !== undefined) {
+      case btnVal === 'CE':
+        this.resetCalculator();
+        break;
+      default:
+        console.log('End of Switch statement')
+    }
+  }
+
+  resetCalculator = (e) => {
+      this.setState({
+        formula: [],
+        memory: '',
+        currentEl: '',
+        result: null,
+        equalityPressed: false
+      })
+  }
+
   render() {
+    const { formula, memory, currentEl } = this.state;
     return (
       <div className={ classes.Calculator }>
           <Header />
-          <Display />
-          <Buttons />
+          <Display 
+              formula={ formula } 
+              memory={ memory } 
+              currentEl={ currentEl } 
+          />
+          <Buttons handleCalculator={ this.handleCalculator } />
       </div>
     );
   }

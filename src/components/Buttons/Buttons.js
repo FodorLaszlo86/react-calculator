f2522562722c6939fae65f6c73c222d29ec2e132
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-03-10 17:38:02 +0100

    memory operand buttons removed exept MC

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -4,19 +4,22 @@ import classes from './Buttons.css';
 
 const btnValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                    '=', '+', '-', '/', '*', '.', '+/-', '%', '√', '1/x',
-                   'C', 'CE', 'MR', 'MS', 'MC', 'M+', 'M-'];
+                   'C', 'CE', 'MC'];
+
 const btnIDs = ['zero', 'one', 'two', 'three', 'four', 'five', 'six',
                 'seven', 'eight', 'nine', 'equals', 'add', 'subtract', 'divide', 'multiply', 'decimal', 'opp',
-                'percent', 'sqrt', 'fraction' ,'clear', 'clearAll', 'memory-recall', 'memory-save', 'memory-clear', 'memory-add', 'memory-substract']
+                'percent', 'sqrt', 'fraction' ,'clear', 'clearAll', 'memory-clear'];
 
 const buttons = ({ handleCalculator }) => {
-    const btnClasses = Object.values(classes).slice(2, -1);
+    const btnClasses = Object.values(classes);
+    const btnClassesSliced = btnClasses.slice(2, btnClasses.length);
+    console.log(btnClassesSliced);
     const allBtn = btnValues.map((btn, i) => {
     return (
         <div 
             key={ `Btn-${ btnIDs[i] }` } 
             id={ btnIDs[i] }
-            className={ `${ classes.Button } ${ btnClasses[i] }` }
+            className={ `${ classes.Button } ${ btnClassesSliced[i] }` }
             onClick={ handleCalculator }
             >
             { btn }
